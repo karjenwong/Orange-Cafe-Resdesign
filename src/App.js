d3f2e0ffd1e1898738633bc0b3678fbf61f4ae5e
Karjen Wong <karjenwong@gmail.com>
2020-01-26 14:49:55 -0800

    installed dependencies for navbar, created dummy components and prototype navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Menu from "./Components/Menu";
+import Gallery from "./Components/Gallery";
+import LinkContainer from "react-router-bootstrap/lib/LinkContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navbar bg="light" expand="md" fixed="bottom" collapseOnSelect>
+            <Navbar.Brand href="#home">Orange Cafe</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <LinkContainer to="/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="about">
+                  <Nav.Link>About</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="menu">
+                  <Nav.Link>Menu</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="gallery">
+                  <Nav.Link>Gallery</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="contact">
+                  <Nav.Link>Contact</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/menu" component={Menu} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
